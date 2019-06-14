c918c46f86caa553a0c132e1bf703e5b5c6a4dfd
simonhettrick <s.hettrick@software.ac.uk>
2019-06-14 15:24:04 +0100

    Starting analysis script

--- analyse_survey.py
+++ analyse_survey.py
@@ -6,6 +6,15 @@ import numpy as np
 import math
 
 
+# Get details for plots from look up table
+from column_name_renaming import col_shortener
+
+
+DATAFILELOC = "./data/"
+DATAFILENAME = "UniSotonSoftwareSurvey-csv.csv"
+STOREFILENAME = "./output/"
+
+
 def import_csv_to_df(location, filename):
     """
     Imports a csv file into a Pandas dataframe
@@ -13,7 +22,7 @@ def import_csv_to_df(location, filename):
     :return: a df
     """
     
-    return pd.read_csv(location + filename + '.csv')
+    return pd.read_csv(location + filename)
 
 
 def export_to_csv(df, location, filename, index_write):
@@ -26,11 +35,29 @@ def export_to_csv(df, location, filename, index_write):
     return df.to_csv(location + filename + '.csv', index=index_write)
 
 
+def rename_cols(df):
+
+    return df.rename(index=str, columns=col_shortener)
+
+def get_counts(df, grouped_cols):
+
+    # Storing results as a dict of dfs
+    univariate_summary_dfs = {}
+
+
+
 def main():
     """
     Main function to run program
     """
 
+    df = import_csv_to_df(DATAFILELOC, DATAFILENAME)
+
+    df = rename_cols(df)
+
+    print(df)
+
+    # get_counts(df,)
 
 
 if __name__ == '__main__':
---
+++ column_name_renaming.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+col_shortener = {
+    'In which faculty are you based?':'faculty',
+    'Which of the following organisations usually fund your research?':'funders',
+    'Simplified job title':'job_title',
+    'Do you use research software?':'use_software',
+    'How important is research software to your work?':'importance_software',
+    'Have you developed your own research software?':'develop_own_code',
+    'How do you rate your software development expertise?':'development_expertise',
+    'Do you feel that you have received sufficient training to develop reliable software?':'training',
+    'Would you be interested in the university helping you commercialise some of your research software?':'want_to_commercialise',
+    'Do you feel that your research software is ready to be shared with a commercial partner?':'ready_to_share',
+    'Have you used IRIDIS, the University\'s high-performance computing (HPC) system?':'hpc_use',
+    'How confident are you with the following technologies? [Version control]':'version_control',
+    'How confident are you with the following technologies? [Continuous integration]':'continuous_integration',
+    'How confident are you with the following technologies? [Unit testing]':'unit_testing',
+    'How would you rate the university\'s current level of support for your software-development needs?':'current_support',
+    'Have you or someone in your group ever hired someone specifically to develop software?':'hired_developer',
+    'Have you ever included costs for software development in a funding proposal?':'funds_for_development',
+    'How suitable would the following models be for your software development needs? [Hire a full-time software developer]':'hire_full_time_developer',
+    'How suitable would the following models be for your software development needs? [Recruit a developer from a central Southampton pool as needed]':'hire_rse'
+}
