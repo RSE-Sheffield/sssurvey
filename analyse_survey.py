8f7c3e7c6fdb5a7d007849f09e416957f9fda51c
Simon Hettrick <s.hettrick@software.ac.uk>
2019-06-14 09:27:32 +0100

    Setting up Python script

---
+++ analyse_survey.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import pandas as pd
+import numpy as np
+import math
+
+
+def import_csv_to_df(location, filename):
+    """
+    Imports a csv file into a Pandas dataframe
+    :params: an xls file and a sheetname from that file
+    :return: a df
+    """
+    
+    return pd.read_csv(location + filename + '.csv')
+
+
+def export_to_csv(df, location, filename, index_write):
+    """
+    Exports a df to a csv file
+    :params: a df and a location in which to save it
+    :return: nothing, saves a csv
+    """
+
+    return df.to_csv(location + filename + '.csv', index=index_write)
+
+
+def main():
+    """
+    Main function to run program
+    """
+
+
+
+if __name__ == '__main__':
+    main()
