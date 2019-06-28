93ae5b36d2b02d3aaea4616a89ce8aed14b5fbad
Simon Hettrick <s.hettrick@software.ac.uk>
2019-06-28 13:50:36 +0100

    Finished off the functions for ordering the results. Changed the count number of responses function, because it needed to be updated to give accurate results for multiple choice questions. Moved the saving out of the summary dfs csvs into its own function.

--- analyse_survey.py
+++ analyse_survey.py
@@ -43,6 +43,21 @@ def export_to_csv(df, location, filename, index_write):
     return df.to_csv(location + filename + '.csv', index=index_write)
 
 
+def remove_timestamp(df):
+    """
+    We don't need the Timestamp col in the analysis but it is nice to have it in the data (in case people want to see
+    how responses came in. Hence I can't remove it in the cleaning stage completed before this analysis, but I'll remove
+    it now.
+    :param df: the main df containing all the data
+    :return: the main df, but without a Timestamp col
+    """
+
+    df.drop(labels=['Timestamp'], axis='columns', inplace=True)
+    print(df.columns)
+
+    return df
+
+
 def clean_col_names(df):
     """
     Takes a dict from a look up file and uses it to rename the columns
@@ -52,26 +67,6 @@ def clean_col_names(df):
     """
 
     return df.rename(index=str, columns=col_shortener)
-
-
-def find_number_responses(df):
-    """
-    Finds the number of people who responded to each question and saves it as a csv
-    :param df: the main dataframe
-    :return: nothing
-    """
-
-    response_list = {}
-
-    for current_col in df.columns:
-        temp_df = df.dropna(axis=0, subset=[current_col])
-        response_list[current_col] = len(temp_df)
-
-    df_responses = pd.DataFrame(response_list.items(), columns=['question','responses'])
-
-    export_to_csv(df_responses, CSVSTORE, 'responses', False)
-
-    return
 
 
 def get_counts(df):
@@ -102,13 +97,35 @@ def get_counts(df):
     return summary_dfs
 
 
+def clean_software_funding(summary_dfs):
+    """
+    Removes the 'No (I\'m not involved in writing funding proposals)' answer from the question on whether people have
+    applied for software funding. In a perfect world, this would have been removed in the pre-analysis data cleaning in
+    OpenRefine, but that's not possible, because this is a multiple choice question that separates multiple answers
+    with semi-colons and OpenRefine can't handle that. The get_counts function deals with the semicolon problem, so now
+    we can finish cleaning the data.
+    :param summary_dfs: dict of dfs holding summaries on the answers to each question
+    :return: dict of dfs holding summaries on the answers to each question - but with the funding question cleaned so
+            people who do not write bids are not included.
+    """
+
+    key = 'funds_for_development'
+
+    df_temp = summary_dfs[key]
+    df_temp.drop(index='No (I\'m not involved in writing funding proposals)', inplace=True)
+    # Obviously, removing one of the options changes the percentages, so have to re-do that calculation
+    df_temp['percentage'] = round(100 * df_temp[key] / df_temp[key].sum(), 0)
+
+    return summary_dfs
+
+
 def change_lows_to_other(summary_dfs):
     """
     Questions with an "other" response can generate lots of low value responses. Convert all answers that gain fewer
      than 5 responses into an "other" category to make things cleaner.
     :param summary_dfs: dict of dfs holding summaries on the answers to each question
     :return: dict of dfs holding summaries on the answers to each question - but with an other response used to mop up
-             ansewrs with low response numbers
+             answers with low response numbers
     """
 
     for key in add_an_other_category:
@@ -130,12 +147,42 @@ def change_lows_to_other(summary_dfs):
     return summary_dfs
 
 
+def find_number_responses(summary_dfs, df):
+    """
+    Finds the number of people who responded to each question and saves it as a csv
+    :param df: the main dataframe
+    :return: nothing
+    """
+
+    response_list = {}
+
+    for key in summary_dfs:
+        df_temp = summary_dfs[key]
+        response_list[key] = df_temp[key].sum()
+
+    # This approach doesn't work for multiple choice questions (you get over-counting for anyone that added more than
+    # one response. For these questions, to get the response number we must go back to the original df
+
+    multi_questions = ['funders', 'funds_for_development']
+
+    for current_question in multi_questions:
+        df_temp = df.dropna(axis=0, subset=[current_question])
+        # Write the actual number of responses over the current number in the response list
+        response_list[current_question] = len(df_temp)
+
+    df_responses = pd.DataFrame(response_list.items(), columns=['question', 'responses'])
+
+    export_to_csv(df_responses, CSVSTORE, 'responses', False)
+
+    return
+
+
 def sort_and_save(summary_dfs):
     """
-    Takes the summary dfs that merely need sorted and saves them out as csvs
-    Uses info from the sort_no_further_analysis var to know which dfs to process
+    Takes the summary dfs that merely need sorted. Uses info from the sort_no_further_analysis var to know which dfs to
+    process
     :param summary_dfs: dict of dfs holding summaries on the answers to each question
-    :return: nothing
+    :return: a better ordered dict of dfs holding summaries on the answers to each question
     """
 
     for key in sort_no_further_analysis:
@@ -143,17 +190,16 @@ def sort_and_save(summary_dfs):
         df_temp.sort_values(by=key, inplace=True, ascending=False)
         # Replace the original df in the dict of dfs
         summary_dfs[key] = df_temp
-        export_to_csv(df_temp, CSVSTORE, key, True)
 
     return summary_dfs
 
 
 def yes_and_no(summary_dfs):
     """
-    Takes summary dfs of yes-and-no questions, sorts them yes first, then no, then saves the csv
-    Uses the yes_no_analysis var to work out which dfs to process
+    Takes summary dfs of yes-and-no questions, sorts them yes first, then no. Uses the yes_no_analysis var to work out
+    which dfs to process
     :param summary_dfs: dict of dfs holding summaries on the answers to each question
-    :return: nothing
+    :return: a better ordered dict of dfs holding summaries on the answers to each question
     """
 
     for key in yes_no_analysis:
@@ -163,43 +209,82 @@ def yes_and_no(summary_dfs):
         df_temp.sort_index(inplace=True, ascending=False)
         # Replace the original df in the dict of dfs
         summary_dfs[key] = df_temp
-        export_to_csv(df_temp, CSVSTORE, key, True)
 
     return summary_dfs
 
 
 def scale_question_analysis(summary_dfs):
+    """
+    To make plotting easy, this sorts the questions with a 1 to 5 answer scale, and saves them back into the dict of
+    dfs.
+    :param summary_dfs: dict of dfs holding summaries on the answers to each question
+    :return: a better ordered dict of dfs holding summaries on the answers to each question
+    """
 
     for key in scale_analysis:
         df_temp = summary_dfs[key]
         df_temp.sort_index(ascending=True, inplace=True)
         # Replace the original df in the dict of dfs
         summary_dfs[key] = df_temp
-        export_to_csv(df_temp, CSVSTORE, key, True)
 
     return summary_dfs
 
+
 def scale_worded_questions(summary_dfs):
+
+    """
+    Some questions have wordy answers that I want to present in a particular order. This puts them in that order using
+    a categorical. I feel that there must be an easier way of doing this, but I can't find it. The function saves the
+    re-ordered dfs back in the summary-dfs.
+    :param summary_dfs: dict of dfs holding summaries on the answers to each question
+    :return: a better ordered dict of dfs holding summaries on the answers to each question
+    """
 
     for key in worded_scale_analysis:
         df_temp = summary_dfs[key]
         # New index makes the current index a col that we can work with
         df_temp.reset_index(inplace=True)
-        print(df_temp)
-        if key == 'version_control' or 'continuous_integration' or 'unit_testing':
-            df_temp['answers'] = pd.Categorical(df_temp['answers'], categories=['Not heard of it', 'Not confident', 'Confident', 'Very confident'], ordered=True)
-            print(df_temp.sort_values('answers'))
-        elif key == 'hire_full_time_developer' or 'hire_rse':
-            df_temp['answers'] = pd.Categorical(df_temp['answers'],
-                                                categories=['Not heard of it', 'Not confident', 'Confident',
-                                                          'Very confident'], ordered=True)
 
-    # No (but we DID expect to write software as part of the project)
-    # No (we did NOT expect to write software as part of the project)
-    # Yes
+        if key in ['version_control', 'continuous_integration', 'unit_testing']:
+            # These categoricals allow you to order a column based on a list. Seems long winded, but it was the only
+            # straightforward way I could find to do it.
+            df_temp['answers'] = pd.Categorical(df_temp['answers'],
+                                                categories=['Not heard of it', 'Not confident','Confident',
+                                                            'Very confident'], ordered=True)
+            df_temp.sort_values('answers', inplace=True)
+
+        elif key == 'funds_for_development':
+            df_temp['answers'] = pd.Categorical(df_temp['answers'],
+                                                categories=['Yes',
+                                                    'No (but we DID expect to write software as part of the project)',
+                                                    'No (we did NOT expect to write software as part of the project)'],
+                                                ordered=True)
+            df_temp.sort_values('answers', inplace=True)
+
+        elif key in ['hire_full_time_developer', 'hire_rse']:
+            df_temp['answers'] = pd.Categorical(df_temp['answers'],
+                                                categories=['Perfect', 'Suitable', 'Unsuitable'], ordered=True)
+            df_temp.sort_values('answers', inplace=True)
+
+        # Now that the hard work's done, we can set the answers col back to be the index and save back into the dict
+        # of dfs
+        df_temp.set_index('answers', inplace=True)
+        summary_dfs[key] = df_temp
 
     return summary_dfs
 
+
+def write_out_summaries(summary_dfs):
+    """
+    Write the summary dfs out to csvs
+    :param summary_dfs: dict of dfs holding summaries on the answers to each question
+    :return: nothing
+    """
+
+    for key in summary_dfs:
+        export_to_csv(summary_dfs[key], CSVSTORE, key, True)
+
+    return
 
 
 def main():
@@ -209,15 +294,19 @@ def main():
 
     df = import_csv_to_df(DATAFILELOC, DATAFILENAME)
 
-    df = clean_col_names(df)
+    df = remove_timestamp(df)
 
-    find_number_responses(df)
+    df = clean_col_names(df)
 
     summary_dfs = get_counts(df)
 
+    summary_dfs = clean_software_funding(summary_dfs)
+
     summary_dfs = change_lows_to_other(summary_dfs)
 
-    # Prepare data for graphing programs
+    find_number_responses(summary_dfs, df)
+
+    # Prepare data for later graphing
 
     summary_dfs = sort_and_save(summary_dfs)
 
@@ -226,6 +315,8 @@ def main():
     summary_dfs = scale_question_analysis(summary_dfs)
 
     summary_dfs = scale_worded_questions(summary_dfs)
+
+    write_out_summaries(summary_dfs)
 
 if __name__ == '__main__':
     main()
