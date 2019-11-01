6565176887647329f116f11b74dfbd734040ccb9
simonhettrick <s.hettrick@software.ac.uk>
2019-11-01 08:49:42 +0000

    Updating report and charts

--- bivariate_instructions.py
+++ bivariate_instructions.py
@@ -3,7 +3,7 @@
 
 which_by_which = {
 
-    'faculty': ['use_software', 'develop_own_code', 'development_expertise', 'training', 'want_to_commercialise', 'version_control', 'funds_for_development'],
+    'faculty': ['use_software', 'training', 'develop_own_code', 'development_expertise', 'training', 'want_to_commercialise', 'version_control', 'funds_for_development'],
     'job_title': ['develop_own_code'],
     'importance_software': ['funds_for_development']
 }
