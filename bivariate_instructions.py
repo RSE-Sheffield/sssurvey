3dbc76b3f700d313f7c260e9b57617b45074c75a
simonhettrick <s.hettrick@software.ac.uk>
2019-12-10 12:59:00 +0000

    Playing with text of report

--- bivariate_instructions.py
+++ bivariate_instructions.py
@@ -3,7 +3,9 @@
 
 which_by_which = {
 
-    'faculty': ['use_software', 'training', 'develop_own_code', 'development_expertise', 'training', 'want_to_commercialise', 'version_control', 'funds_for_development'],
-    'job_title': ['develop_own_code'],
-    'importance_software': ['funds_for_development']
+    'faculty': ['version_control', 'unit_testing', 'continuous_integration']
+
+#    'faculty': ['use_software', 'training', 'develop_own_code', 'development_expertise', 'training', 'want_to_commercialise', 'version_control', 'funds_for_development'],
+#    'job_title': ['develop_own_code'],
+#    'importance_software': ['funds_for_development']
 }
