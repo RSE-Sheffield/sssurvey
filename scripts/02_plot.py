c41b11dac2b9403e178090a13f95728043d957b0
bobturneruk <r.d.turner@sheffield.ac.uk>
2021-05-25 11:57:22 +0100

    initial plotting

---
+++ scripts/02_plot.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+from plotnine import *
+
+sheffield_df = pd.read_csv('./data/clean/sheffield_clean.csv')
+
+# Response by faculty
+(ggplot(sheffield_df, aes(x='faculty')) + 
+    geom_bar() + 
+    coord_flip()
+).save('./plots/01_faculty_plot.png')
+
+# Software use
+(ggplot(sheffield_df, aes(x='use_software')) + 
+    geom_bar() + 
+    coord_flip()
+).save('./plots/04_use_software.png')
