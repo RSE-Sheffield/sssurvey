d871828546ca86ffe5fb6627039a4e5b47a1b481
bobturneruk <r.d.turner@sheffield.ac.uk>
2021-05-25 12:34:00 +0100

    begin manual cleanup

---
+++ scripts/00_make_cleanup_helpers.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from pandas.core.algorithms import unique
+
+# Get details for plots from look up table
+from column_name_renaming import col_shortener
+
+wip_df = pd.read_csv('./data/raw/sheffield.csv')
+
+# Rename columns
+wip_df.rename(index=str, columns=col_shortener, inplace=True)
+
+# Get unqiue self described job titles
+unique_jobs = wip_df['job_title'].unique()
+pd.DataFrame.from_dict({'raw_job' : unique_jobs, 'clean_job' : [None] * unique_jobs.size}).to_csv('./data/working/unique_job_list.csv', index=False)
